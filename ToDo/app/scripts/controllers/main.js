e8384ef5a5005b2d26b5dbe8ca237fc757517e51
Wynand Kok <wynand.kok@live.com>
2013-10-23 18:50:33 +0200

    CR_D functionality; LESS dir

--- ToDo/app/scripts/controllers/main.js
+++ ToDo/app/scripts/controllers/main.js
@@ -1,17 +1,32 @@
 'use strict';
 
 angular.module('ToDoApp')
-  .controller('MainCtrl', function ($scope, $location, todoStorage) {
+  .controller('MainCtrl', function ($scope, $location, todoStorage, filterFilter) {
   	var todos = $scope.todos = todoStorage.get();
 
   	$scope.newTodo = '';
   	$scope.editedTodo = null;
 
   	$scope.$watch('todos', function(newValue, oldValue) {
+  		$scope.remainingCount = filterFilter(todos, { completed: false }).length;
+  		$scope.completedCount = todos.length - $scope.remainingCount;
+  		$scope.allChecked = !$scope.remainingCount;
   		if (newValue !== oldValue) {
   			todoStorage.put(todos);
   		}
   	}, true);
+
+  	if ($location.path() === '') {
+  		$location.path('/');
+  	}
+
+  	$scope.location = $location;
+
+  	$scope.$watch('location.path()', function(path) {
+  		$scope.statusFilter = (path === '/active') ?
+  				{ completed: false } : (path === '/completed') ?
+  				{ completed: true } : null;
+  	});
 
   	$scope.addToDo = function() {
   		var newTodo = $scope.newTodo.trim();
@@ -26,11 +41,19 @@ angular.module('ToDoApp')
   		$scope.newTodo = '';
   	}
 
-  	$scope.markAll = function(completed) {
-  		todos.foreach(function(todo) {
-  			todo.completed = completed;
+  	$scope.removeToDo = function(todo) {
+  		todos.splice(todos.indexOf(todo), 1);
+  	}
+
+  	$scope.clearCompletedTodos = function() {
+  		$scope.todos = todos = todos.filter(function(val) {
+  			return !val.completed;
   		});
   	};
 
-
+  	$scope.markAll = function(completed) {
+  		todos.forEach(function(todo) {
+  			todo.completed = completed;
+  		});
+  	};
   });
